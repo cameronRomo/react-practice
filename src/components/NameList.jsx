3571c89e70a7749f6bc1de32f694fcd7765a2206
Cameron Romo <cromo1225@gmail.com>
2023-01-09 14:31:48 -0700

    Add simple search for names

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -6,23 +6,50 @@ export default class NameList extends React.Component {
   constructor() {
     super();
     this.sate = {
-      names: []
+      names: [],
+      filterText: ""
     }
   }
-
+  
   componentWillMount() {
     this.setState({
       names: this.props.names
     })
   }
+  
+  handleClick = (e) => {
+    e.preventDefault();
+  }
+  
+  nameFilter = (e) => {
+    this.setState({
+      filterText: e.target.value
+    })
+  }
 
+  handleBlur = () => {
+    let {filterText, names} = this.state;
+    if (filterText.length >= 3) {
+      let filteredNames = _.filter(names, (name) => {
+        let fullName = `${name.first_name} ${name.last_name}`
+        return fullName.toLowerCase().includes(filterText.toLowerCase());
+      })
+      this.setState({
+        names: filteredNames
+      })
+    }
+  }
+  
   render() {
     let {names} = this.state;
 
     return (
       <section>
+        <form onBlur={this.handleBlur}>
+          <input type='text' onChange={this.nameFilter}/>
+        </form>
         {_.map(names, (name) => (
-          <h2>{name.first_name} {name.last_name}</h2>
+          <h2 key={name.first_name}><a href='#' onClick={this.handleClick}>{name.first_name} {name.last_name}</a></h2>
         ))}
       </section>
     )
